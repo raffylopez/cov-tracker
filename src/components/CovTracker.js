67a22a90d9d7c83d4c2c6a18b3b84e6bdb89a9fa
raffylopez <raffylopez@gmail.com>
2020-11-29 11:52:26 +0800

    Add CovTracker component

---
+++ src/components/CovTracker.js
@@ -0,0 +1,163 @@
+/*
+ * CovTracker.js
+ * Copyright (C) 2020 volare <volare@CASSIOPEIA.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+import React from "react";
+import fetchJsonFromUrl from "../util/fetchJsonFromUrl.js";
+import CountryList from "./CountryList";
+import StatsSheet from "./StatsSheet";
+import StatusDisplay from "./StatusDisplay";
+import ChartDisplay from "./ChartDisplay";
+import cx from "classnames";
+import getMonthsList from "../data/getMonthsList.js";
+
+import "./CovTracker.css";
+
+export default class CovTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      statusMessage: "Loading...",
+      width: window.innerWidth - 150,
+      height: 400,
+      data: [],
+      stats: {},
+      selectedCountry: {},
+      countryData: { TotalConfirmed: 0 },
+      inProgress: true,
+      isInitialAvailable: false,
+      countries: [
+        {
+          Country: "",
+          Slug: "",
+          CountryCode: "",
+        },
+      ],
+    };
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.updateDimensions = this.updateDimensions.bind(this);
+    this.findPerCountryStats = this.findPerCountryStats.bind(this);
+  }
+
+  findPerCountryStats(selectedCountry) {
+    const dataComposerCallback = (jsonData) => {
+      if (jsonData.length === 0) {
+        const newData = [0, 0].map((e) => {
+          return { name: 0, Confirmed: 0, Recovered: 0 };
+        });
+        this.setState({ data: newData });
+        return;
+      }
+      const newData = jsonData.map((e, idx) => {
+        return {
+          name: e.Date,
+          Confirmed: e.Confirmed,
+          Recovered: e.Recovered,
+        };
+      });
+      this.setState({ data: newData, inProgress: false });
+    };
+
+    fetchJsonFromUrl(
+      `https://api.covid19api.com/total/dayone/country/${selectedCountry.slug}`,
+      dataComposerCallback
+    ).catch((error) => {
+      this.setState({
+        statusMessage:
+          "Unable to reach endpoint at this time. Please try again in a while.",
+      });
+    });
+  }
+
+  onChangeHandler() {
+    const elemSelectCountry = document.getElementById("select-country");
+
+    const selectedCountry = {
+      name: elemSelectCountry.options[elemSelectCountry.selectedIndex].text,
+      slug: elemSelectCountry.value, // dropdown element value is country slug
+    };
+
+    const countryData = this.state.countries.find(
+      (element) => element.Slug == selectedCountry.slug
+    );
+    this.setState({
+      countryData: countryData,
+      selectedCountry: selectedCountry,
+      inProgress: true,
+    });
+    this.findPerCountryStats(selectedCountry);
+  }
+
+  updateDimensions() {
+    this.setState({ width: window.innerWidth - 150 });
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+
+    fetchJsonFromUrl("https://api.covid19api.com/summary", (dataAsJson) => {
+      const filteredCountries = dataAsJson.Countries.filter((element) => {
+        return element.TotalConfirmed != 0 && element.TotalRecovered != 0;
+      });
+      this.setState({
+        countries: filteredCountries,
+        selectedCountry: { name: "Philippines", slug: "philippines" },
+        isInitialAvailable: true,
+      });
+    })
+      .then(() => this.onChangeHandler())
+      .catch((error) => {
+        this.setState({
+          statusMessage:
+            "Unable to reach endpoint at this time. Please try again in a while.",
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  render() {
+    const {
+      data,
+      inProgress,
+      countries,
+      selectedCountry,
+      countryData,
+      isInitialAvailable,
+      statusMessage,
+      width,
+      height,
+    } = this.state;
+    return (
+      <div>
+        <StatusDisplay inProgress={inProgress} statusMessage={statusMessage} />
+        <div
+          className={cx({
+            covvisible: isInitialAvailable,
+            covhidden: !isInitialAvailable,
+          })}
+        >
+          <h1 className={"pageTitle"}>
+            <span>Tracker: </span>CoV Infection Curve Per Country
+          </h1>
+          <CountryList
+            onChangeHandler={this.onChangeHandler}
+            selectedCountry={selectedCountry}
+            countries={countries}
+          />
+          <StatsSheet
+            countryData={countryData}
+            selectedCountry={selectedCountry}
+            inProgress={inProgress}
+          />
+          <ChartDisplay data={data} width={width} height={height} />
+        </div>
+      </div>
+    );
+  }
+}
